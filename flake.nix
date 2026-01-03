fa69b2ce1e1c50a292016dcc7ffa7434c907a20e
taiseiue <taiseiue@wsnet.jp>
2026-01-03 23:19:34 +0900

    つかえる範囲をunixに絞る/lib.getExeに頼る

--- flake.nix
+++ flake.nix
@@ -1,9 +1,7 @@
 {
   description = "qrg - QR code cli generator";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
     let
@@ -22,55 +20,55 @@
       );
 
       commit = self.rev or "dirty";
+
       date =
         let d = self.lastModifiedDate or "19700101";
         in "${builtins.substring 0 4 d}-${builtins.substring 4 2 d}-${builtins.substring 6 2 d}T00:00:00Z";
-      
+
       version =
         if self ? rev
-        then "unstable-${builtins.substring 0 8 (self.rev)}"
+        then "unstable-${builtins.substring 0 8 self.rev}"
         else "dev";
+
+      mkQrg = pkgs: pkgs.buildGoModule {
+        pname = "qrg";
+        inherit version;
+        src = self;
+
+        vendorHash = "sha256-wzMLu5HV2Ypebjlc+M4G2n54idbPJE0UZN9KxxehCsE=";
+
+        # go build flags
+        buildFlagsArray = [ "-trimpath" ];
+
+        ldflags = [
+          "-s" "-w"
+          "-X" "github.com/rokuosan/qrg/cmd.version=${version}"
+          "-X" "github.com/rokuosan/qrg/cmd.commit=${commit}"
+          "-X" "main.date=${date}"
+          "-X" "main.builtBy=nix"
+        ];
+
+        env.CGO_ENABLED = "0";
+
+        meta = with lib; {
+          description = "QR code CLI generator";
+          homepage = "https://github.com/rokuosan/qrg";
+          license = licenses.mit;
+          mainProgram = "qrg";
+          platforms = platforms.unix;
+        };
+      };
     in
     {
-      packages = forAllSystems ({ pkgs, system }: rec {
-        qrg = pkgs.buildGoModule {
-          pname = "qrg";
-          inherit version;
-
-          src = self;
-
-          vendorHash = "sha256-wzMLu5HV2Ypebjlc+M4G2n54idbPJE0UZN9KxxehCsE=";
-
-          flags = [ "-trimpath" ];
-
-          ldflags = [
-            "-s" "-w"
-            "-X" "github.com/rokuosan/qrg/cmd.version=${version}"
-            "-X" "github.com/rokuosan/qrg/cmd.commit=${commit}"
-            "-X" "main.date=${date}"
-            "-X" "main.builtBy=nix"
-          ];
-
-          env = {
-            CGO_ENABLED = "0";
-          };
-
-          meta = with lib; {
-            description = "QR code CLI generator";
-            homepage = "https://github.com/rokuosan/qrg";
-            license = licenses.mit;
-            mainProgram = "qrg";
-            platforms = platforms.all;
-          };
-        };
-
+      packages = forAllSystems ({ pkgs, ... }: rec {
+        qrg = mkQrg pkgs;
         default = qrg;
       });
 
-      apps = forAllSystems ({ system, ... }: {
+      apps = forAllSystems ({ pkgs, system, ... }: {
         default = {
           type = "app";
-          program = "${self.packages.${system}.qrg}/bin/qrg";
+          program = pkgs.lib.getExe self.packages.${system}.qrg;
         };
       });
 
